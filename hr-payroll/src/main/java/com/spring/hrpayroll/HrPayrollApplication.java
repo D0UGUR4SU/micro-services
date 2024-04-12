21045afaacba47504dc03b7740757a9a0fd4de5e
D4GUR4SU <douglas_search@hotmail.com>
2024-04-12 13:51:41 -0300

    Feign client

--- hr-payroll/src/main/java/com/spring/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/spring/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.spring.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients
 @SpringBootApplication
 @ComponentScan("com.spring.hrpayroll.entities.Payment")
 public class HrPayrollApplication {
