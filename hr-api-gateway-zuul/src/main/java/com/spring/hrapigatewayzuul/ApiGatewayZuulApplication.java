0c506e80006033d21f771042378d51d6e63d7153
D4GUR4SU <douglas_search@hotmail.com>
2024-04-13 11:28:10 -0300

    Project hr-api-gateway-zuul created

--- hr-api-gateway-zuul/src/main/java/com/spring/hrapigatewayzuul/ApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/spring/hrapigatewayzuul/ApiGatewayZuulApplication.java
@@ -2,14 +2,14 @@ package com.spring.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
 	}
 
 }
