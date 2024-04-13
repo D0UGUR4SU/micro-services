61b077401722f409e4732f2c8f9ae4d02e5eb307
D4GUR4SU <douglas_search@hotmail.com>
2024-04-13 14:39:57 -0300

    Databse seeding

--- hr-user/src/main/java/com/spring/hruser/config/AppConfig.java
+++ hr-user/src/main/java/com/spring/hruser/config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.spring.hruser.config;public class AppConfig {
+package com.spring.hruser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -1,5 +1,15 @@
-INSERT INTO tb_user (name, email, password) VALUES ("Bob", "bob@email.com", "123456");
-INSERT INTO tb_user (name, email, password) VALUES ("Maria", "maria@email.com", "123456");
-INSERT INTO tb_user (name, email, password) VALUES ("John", "john@email.com", "123456");
-INSERT INTO tb_user (name, email, password) VALUES ("Chuck", "chuck@email.com", "123456");
-INSERT INTO tb_user (name, email, password) VALUES ("Barry", "barry@email.com", "123456");
+INSERT INTO tb_user (name, email, password) VALUES ('Bob', 'bob@email.com', '$2a$12$8oCXyJ.x8Ily4p3WkbNa5e4BJQJwlCOgpdvtDhA9Nnamc/dmxmXba');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@email.com', '$2a$12$8oCXyJ.x8Ily4p3WkbNa5e4BJQJwlCOgpdvtDhA9Nnamc/dmxmXba');
+INSERT INTO tb_user (name, email, password) VALUES ('John', 'john@email.com', '$2a$12$8oCXyJ.x8Ily4p3WkbNa5e4BJQJwlCOgpdvtDhA9Nnamc/dmxmXba');
+INSERT INTO tb_user (name, email, password) VALUES ('Chuck', 'chuck@email.com', '$2a$12$8oCXyJ.x8Ily4p3WkbNa5e4BJQJwlCOgpdvtDhA9Nnamc/dmxmXba');
+INSERT INTO tb_user (name, email, password) VALUES ('Barry', 'barry@email.com', '$2a$12$8oCXyJ.x8Ily4p3WkbNa5e4BJQJwlCOgpdvtDhA9Nnamc/dmxmXba');
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMINISTRATOR');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (4, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (5, 2);
+
