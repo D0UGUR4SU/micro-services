a92ebee8bc59a8646349bb0f2afc8c905f517586
D4GUR4SU <douglas_search@hotmail.com>
2024-04-13 14:07:36 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/spring/hruser/HrUser.java
@@ -0,0 +1,13 @@
+package com.spring.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUser {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUser.class, args);
+	}
+
+}
