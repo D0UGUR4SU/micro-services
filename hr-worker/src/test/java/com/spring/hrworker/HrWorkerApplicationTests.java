f461cb21a82134a5ad55a8d8751d8b77c8707933
D4GUR4SU <douglas_search@hotmail.com>
2024-04-13 21:45:04 -0300

    hr-worker container

--- hr-worker/src/test/java/com/spring/hrworker/HrWorkerApplicationTests.java
+++ hr-worker/src/test/java/com/spring/hrworker/HrWorkerApplicationTests.java
@@ -3,7 +3,7 @@ package com.spring.hrworker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = { HrWorkerApplicationTests.class })
 class HrWorkerApplicationTests {
 
 	@Test
